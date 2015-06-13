f9ec18a77d794ebc895a5f16745c956d6f0e5ec3
Yuri Neves Silveira <yurinevessilveira@gmail.com>
2015-06-12 21:42:55 -0300

    fetch

---
+++ test/sample/sample.js
@@ -0,0 +1,26 @@
+// - -------------------------------------------------------------------- - //
+
+"use strict";
+
+var fs = require("fs");
+var Crawler = require("../../../node-bauer-crawler");
+
+var crawler = new Crawler();
+
+crawler.require(__dirname + "/../../");
+
+crawler.ready(function() {
+  
+  this.promise()
+    .fetch("http://yneves.com/")
+    .then(function(val) {
+      fs.unlinkSync(val.file);
+    })
+    .exit();
+  
+  
+});
+
+crawler.start();
+
+// - -------------------------------------------------------------------- - //
--- test/test-fetch.js
+++ test/test-fetch.js
@@ -2,26 +2,34 @@
 
 "use strict";
 
+var cp = require("child_process");
 var assert = require("assert");
 
 // - -------------------------------------------------------------------- - //
 
 describe("Fetch",function() {
 
-  it("promise",function() {
+  it("require",function() {
     require(__dirname + "/../lib/promise.js");
-  });
-  
-  it("master",function() {
     require(__dirname + "/../lib/master.js");
-  });
-  
-  it("worker",function() {
     require(__dirname + "/../lib/worker.js");
+    require(__dirname + "/../lib/index.js");
   });
   
-  it("index",function() {
-    require(__dirname + "/../lib/index.js");
+  it("fetch",function(done) {
+    var proc = cp.spawn("node",[__dirname + "/sample/sample.js"],{ stdio: "pipe" });
+    var output = "";
+    proc.stdout.on("data",function(data) {
+      output += data.toString("utf8");
+    });
+    var error = "";
+    proc.stderr.on("data",function(data) {
+      error += data.toString("utf8");
+    });
+    proc.on("exit",function() {
+      assert.equal(error,"");
+      done();
+    });
   });
   
 });
